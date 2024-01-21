f1c83a2cfd10ad9d6a5e01401678ff62339075fa
ShariarSheikh <shariar.dev@gmail.com>
2024-01-22 04:17:22 +0600

    added server production cors url

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://blogify-assessment-test-server.onrender.com',
+    ],
+  });
   await app.listen(8000);
 }
 bootstrap();
